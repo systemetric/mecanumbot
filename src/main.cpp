f7458363ead5008b7b99db7ed91365627f0fecd7
casheww <51793722+casheww@users.noreply.github.com>
2022-07-26 20:56:48 +0100

    fix motor order

--- src/main.cpp
+++ src/main.cpp
@@ -5,23 +5,34 @@
  * A and C pins (left-hand motors) are defined by the two pairs of jumpers (each pair has a red wire and black wire) on the bottom board.
  * They link the arduino's digital pins to motor pins (near the screw terminals that the motor power wires come from) 
  *                                                    (they're confusingly labelled 6,7,4,5)
- * I've plugged them into 12,11,3,2 respectively.
  * 
  * B and D pins (right-hand motors) are defined by the little 1x2 pin connectors that tie adjacent pins together on the top board.
  * They basically let you make a binary choice between two pre-defined arduino digital pins for each pin. 
- * I have them set to 6,7,4,5.
  * (careful, these connectors are fragile. You can grab the inner and outer edges of one side with some tweasers.)
  * 
  * When fiddling with this, as always, avoid pins used for built-in purposes. 
+ * 
+ *    front
+ * A ------ B
+ * | ard    |
+ * | uino   |
+ * |        |   right
+ * |    batt|
+ * |     ery|
+ * C -----  D
+ * 
  */
 #define A_DIR 12    // A : front left
 #define A_PWM 11
-#define B_DIR  7    // B : front right
-#define B_PWM  6
+#define B_DIR  4    // B : front right
+#define B_PWM  5
 #define C_DIR  2    // C : back left
 #define C_PWM  3
-#define D_DIR  4    // D : back right
-#define D_PWM  5
+#define D_DIR  7    // D : back right
+#define D_PWM  6
+
+int dirPins[4] = {A_DIR, B_DIR, C_DIR, D_DIR};
+int pwmPins[4] = {A_PWM, B_PWM, C_PWM, D_PWM};
 
 
 void setup() {
@@ -54,6 +65,23 @@ void stopDriving() {
   digitalWrite(B_PWM, 0);
   digitalWrite(C_PWM, 0);
   digitalWrite(D_PWM, 0);
+}
+
+void doMotorTest(int p, int t) {
+  for (int i = 0; i < sizeof(dirPins)/sizeof(dirPins[0]); i++) {
+    int dir = dirPins[i];
+    int pwm = pwmPins[i];
+    setMotorDir(dir, true);
+    analogWrite(pwm, p);
+    delay(t);
+    stopDriving();
+    delay(t/5);
+    setMotorDir(dir, false);
+    analogWrite(pwm, p);
+    delay(t);
+    stopDriving();
+    delay(t/5);
+  }
 }
 
 /// neg power for backwards, pos power for forwards
@@ -89,7 +117,7 @@ void moveX(int power) {
   analogWrite(D_PWM, power);
 }
 
-int p = 50;;
+int p = 100;
 int t = 500;
 
 void loop() {
